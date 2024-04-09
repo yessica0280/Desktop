d546baa25fe14f1548e702304baf00e2882c2553
yessicaperez2 <yp80905@gmail.com>
2024-04-09 12:31:09 -0500

    hola

--- PerezMachuca_YessicaAndrea/Pytho_PerezYessica/DIA 1/yessica.py
+++
@@ -1,8 +0,0 @@
-#-----------------------------------------------
-#-----------DIA CHEAT SHEET PYTHON--------------
-#-----------------------------------------------
-
-#Imprimir hola mundo
-print("hola mundo.")
-
-#Desarrollado por Yessica Andrea Perez Machuca C.C: 1005044824
---
+++ PerezMachuca_YessicaAndrea/Python1_PerezYessica/DIA 1/yessica.py
@@ -0,0 +1,27 @@
+#-----------------------------------------------
+#-----------DIA CHEAT SHEET PYTHON--------------
+#-----------------------------------------------
+
+#Imprimir hola mundo
+print("hola mundo.")
+
+#datos primitivos
+
+#numero
+numerito = 1 #nombrevariable = valor
+print(numerito)#imprimir(variable)
+print(type(numerito))#imprimir(tipo(variable))
+
+#Decimal
+numeritoDecimal=1.1
+print(numeritoDecimal)
+print(type8numeritoDecimal)
+
+#Booleano
+mibooleanito = True
+print(miBooleanito)
+print(type(mibooleanito))
+
+
+
+#Desarrollado por Yessica Andrea Perez Machuca C.C: 1005044824
