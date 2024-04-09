fb898190f4f68cc9e8b7efdf8e6fa33dbbcaa1cf
YessicaPerez1 <yp80905@gmail.com>
2024-04-09 12:08:06 -0500

    holis

---
+++ PerezMachuca_YessicaAndrea/Pytho_PerezYessica/DIA 1/yessica.py
@@ -0,0 +1,8 @@
+#-----------------------------------------------
+#-----------DIA CHEAT SHEET PYTHON--------------
+#-----------------------------------------------
+
+#Imprimir hola mundo
+print("hola mundo.")
+
+#Desarrollado por Yessica Andrea Perez Machuca C.C: 1005044824
