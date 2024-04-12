9d629843e09da10b8c98797843e074518cf9ecbc
yessica <yp80905@gmail.com>
2024-04-12 16:30:55 -0500

    holis

---
+++ PerezMachuca_YessicaAndrea/Python1_PerezYessica/DIA 4/Ejercicio4_PerezYessica.py
@@ -0,0 +1,28 @@
+#ejercicio4
+print("Bienvenidos a nuestro programa")
+print("")
+plata = int(input("el dinero debe ser en euros"))
+canti10 = plata // 10
+plata = plata % 10
+print("")
+canti5 = plata // 5
+plata = plata % 5
+print("")
+canti1 = plata // 1
+plata == plata % 1
+print("")
+print("aqui podemos ver cuantas monedas de 10 euros necesitamos", canti10)
+print("aqui podemos ver cuantas monedas de 5 euros necesitamos", canti5)
+print("aqui podemos ver cuantas monedas de 1 euros necesitamos", canti1)
+
+#Desarrollado por Yessica Andrea Perez Machuca C.C 1005044824 Grupo:T2
+
+
+ 
+  
+  
+ 
+ 
+ 
+
+
